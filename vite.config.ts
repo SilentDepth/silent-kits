faa509b1f64ab390e46ba0c9cc8269673e13e2f9
SilentDepth <silentdepth@outlook.com>
2021-02-08 16:32:16 +0800

    chore: use localhost domain during dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import jsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
   plugins: [vue(), jsx()],
+  server: {
+    host: 'kits.silent.localhost',
+  },
+  build: {
+    brotliSize: false,
+  },
 })
